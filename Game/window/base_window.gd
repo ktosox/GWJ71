20c057893bcf61a53f9070cca46d1818e6e6788f
Ktosox <ktoso3@gmail.com>
2024-07-18 10:44:54 +0200

    starting work on inventory and swapping mini game

--- Game/window/base_window.gd
+++ Game/window/base_window.gd
@@ -3,6 +3,7 @@ extends Window
 var bar_button_scene = preload("res://window/bar_button.tscn")
 
 
+
 func _ready():
 	create_bar_item()
 	pass
@@ -11,6 +12,7 @@ func _ready():
 func create_bar_item():
 	var new_bar_button = bar_button_scene.instantiate()
 	new_bar_button.connect("pressed",Callable(self,"re_open"))
+	new_bar_button.text = get_child(0).name
 	GM.place_bar_button(new_bar_button)
 	pass
 
